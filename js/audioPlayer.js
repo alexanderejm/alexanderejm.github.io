c0864bf57df57b41c6959e460dd99b9f1d4041dc
alexanderejm <alexanderenriquejm@gmail.com>
2020-07-12 18:58:09 -0300

    fix to media player

--- js/audioPlayer.js
+++ js/audioPlayer.js
@@ -12,37 +12,38 @@ x.onreadystatechange = function () {
             document.getElementById("cover").src = image;
             document.getElementById("audio").src = enclosure;
             document.getElementById("audio").type = 'audio/mp3';
-            
+
             madeBy.innerText = creator;
             audioTitle.innerText = title;
         }
-    }
-    var audio = {
-        init: function () {
-            var $that = this;
-            $(function () {
-                $that.components.media();
-            });
-        },
-        components: {
-            media: function (target) {
-                var media = $('audio.fc-media', (target !== undefined) ? target : 'body');
-                if (media.length) {
-                    media.mediaelementplayer({
-                        audioHeight: 40,
-                        features: ['playpause', 'current', 'duration', 'progress', 'volume', 'tracks', 'fullscreen'],
-                        alwaysShowControls: true,
-                        timeAndDurationSeparator: '<span></span>',
-                        iPadUseNativeControls: true,
-                        iPhoneUseNativeControls: true,
-                        AndroidUseNativeControls: true
-                    });
-                }
+        var audio = {
+            init: function () {
+                var $that = this;
+                $(function () {
+                    $that.components.media();
+                });
             },
+            components: {
+                media: function (target) {
+                    var media = $('audio.fc-media', (target !== undefined) ? target : 'body');
+                    if (media.length) {
+                        media.mediaelementplayer({
+                            audioHeight: 40,
+                            features: ['playpause', 'current', 'duration', 'progress', 'volume', 'tracks', 'fullscreen'],
+                            alwaysShowControls: true,
+                            timeAndDurationSeparator: '<span></span>',
+                            iPadUseNativeControls: true,
+                            iPhoneUseNativeControls: true,
+                            AndroidUseNativeControls: true
+                        });
+                    }
+                },
 
-        },
-    };
-    audio.init();
+            },
+        };
+        audio.init();
+    }
+
 };
 x.send(null);
 
